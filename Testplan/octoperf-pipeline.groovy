fd76a9c63c7db3961a2a216f5342fa018f9e8e1b
mounicagorla <92096684+mounicagorla@users.noreply.github.com>
2021-10-14 12:58:44 +0530

    Update octoperf-pipeline.groovy

--- Testplan/octoperf-pipeline.groovy
+++ Testplan/octoperf-pipeline.groovy
@@ -16,7 +16,7 @@ node {
     }
 
     stage('Execute Performance Tests') {
-     bat " cd c:/apache-jmeter/apache-jmeter/bin/jmeter.bat -n -t submitMaterialOrder.jmx -l testreport.jtl"
+     build job: 'jmeterdemo'
     }
 
     stage('Analyse Results') {
