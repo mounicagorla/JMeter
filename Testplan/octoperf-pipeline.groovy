d9827a71c8c40369dc7c4975fb00f48ffa55ffd8
mounicagorla <92096684+mounicagorla@users.noreply.github.com>
2021-11-12 17:48:46 +0530

    Update octoperf-pipeline.groovy

--- Testplan/octoperf-pipeline.groovy
+++ Testplan/octoperf-pipeline.groovy
@@ -16,7 +16,7 @@ node {
     }
 
     stage('Execute Performance Tests') {
-          bat  'C:/apache-jmeter-5.4.1/apache-jmeter-5.4.1/bin/jmeter.bat -n -t  Testplan/Controller.jmx -Jclientid=nike.niketech.edge-mgorla -Jclientsecret=T8sgW_b00dK2e1oEcQ2awYY-orID4mE3b7TByQxV5yjgj4iewL4euk2bNYd6brZO -l Testplan/testreport1.jtl'
+          bat  'C:/apache-jmeter-5.4.1/apache-jmeter-5.4.1/bin/jmeter.bat -n -t  Testplan/Controller.jmx -Jusers1=5 -jusers2=6 -Jclientid=nike.niketech.edge-mgorla -Jclientsecret=T8sgW_b00dK2e1oEcQ2awYY-orID4mE3b7TByQxV5yjgj4iewL4euk2bNYd6brZO -l Testplan/testreport.jtl'
     }
     
     stage('Publish Report') {
